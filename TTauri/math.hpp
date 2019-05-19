28e80e6b5bce8e4868eacd52cdb50f1fdb4c381d
Take Vos <info@takevos.nl>
2019-05-19 16:28:44 +0200

    Add number of points to cubic solver

--- TTauri/math.hpp
+++ TTauri/math.hpp
@@ -17,7 +17,6 @@ constexpr float oneThird = 1.0f / 3.0f;
 constexpr float oneForth = 1.0f / 4.0f;
 constexpr float oneTwentySeventh = 1.0f / 27.0f;
 
-
 /*! Trigonometric solution for three real roots
  */
 inline std::tuple<float,float,float> solveDepressedCubicTrig(float const p, float const q) {
@@ -41,48 +40,58 @@ inline float solveDepressedCubicCardano(float const p, float const q, float cons
 /*! Solve cubic function in the form.
  * t*t*t + p*t + q = 0
  */
-inline std::tuple<float,float,float> solveDepressedCubic(float p, float q) {
-    float t0, t1, t2;
+inline std::tuple<int8_t,float,float,float> solveDepressedCubic(float p, float q) {
+    int8_t count = 0;
+    float t0 = 0.0f;
+    float t1 = 0.0f;
+    float t2 = 0.0f;
 
-    if (p == 0.0f && q == 0.0f) {
-        t0 = t1 = t2 = 0.0f;
-
-    } else {
+    if (p != 0.0f || q != 0.0f) {
         auto const D = oneForth*q*q + oneTwentySeventh*p*p*p;
 
         if (D < 0) {
             // Has three real roots.
             std::tie(t0, t1, t2) = solveDepressedCubicTrig(p, q);
+            count = 3;
 
         } else if (D == 0.0) {
             // Has two real roots, or maybe one root
             t0 = (3.0f*q) / p;
-            t1 = t2 = (-3.0f*q) / (2.0f*p);
+            t1 = (-3.0f*q) / (2.0f*p);
+            count = 2;
 
         } else {
             // Has one real root.
-            t0 = t1 = t2 = solveDepressedCubicCardano(p, q, D);
+            t0 = solveDepressedCubicCardano(p, q, D);
+            count = 1;
         }
     }
 
-    return { t0, t1, t2 };
+    return { count, t0, t1, t2 };
 }
 
 /*! Solve cubic function.
  * a*x*x*x + b*x*x + c*x + d = 0
  */
-inline std::tuple<float,float,float> solveCubic(float a, float b, float c, float d) {
-    auto const p = (3.0f*a*c - b*b) / (3.0f*a*a);
-    auto const q = (2.0f*b*b*b - 9.0f*a*b*c + 27.0f*a*a*d) / (27.0f*a*a*a);
+inline std::tuple<int8_t,float,float,float> solveCubic(float a, float b, float c, float d) {
+    if (a == 0.0f) {
+        auto const [count, r0, r1] = solveQuadratic(b, c, d);
+        return { count, r0, r1, 0.0f };
 
-    auto const [t0, t1, t2] = solveDepressedCubic(p, q);
+    } else {
+        auto const p = (3.0f*a*c - b*b) / (3.0f*a*a);
+        auto const q = (2.0f*b*b*b - 9.0f*a*b*c + 27.0f*a*a*d) / (27.0f*a*a*a);
 
-    auto const b_3a = b / (3.0f*a);
-    return {
-        t0 - b_3a,
-        t1 - b_3a,
-        t2 - b_3a
-    };
+        auto const [count, t0, t1, t2] = solveDepressedCubic(p, q);
+
+        auto const b_3a = b / (3.0f*a);
+        return {
+            count,
+            t0 - b_3a,
+            t1 - b_3a,
+            t2 - b_3a
+        };
+    }
 }
 
 inline float viktorCross(glm::vec2 const a, glm::vec2 const b) {
