785831a84cccdc3342f3cd49d4daa901961d27b0
Take Vos <info@takevos.nl>
2019-06-13 11:14:59 +0200

    Rename files

--- TTauri/Draw/TTauriIconParser.cpp
+++ TTauri/Draw/TTauriIconParser.cpp
@@ -15,24 +15,6 @@ struct big_fpoint1_13_buf_t {
     }
 }
 
-
-struct Header {
-    big_uint8_buf_t magic[4];
-    big_uint16_buf_t numberOfIcons;
-};
-
-struct Icon {
-    big_uint16_buf_t numberOfContours;
-};
-
-struct Contour {
-    big_uint8_buf_t red;
-    big_uint8_buf_t green;
-    big_uint8_buf_t blue;
-    big_uint8_buf_t alpha;
-    big_uint16_buf_t numberOfPoints;
-};
-
 struct Point {
     enum class Type { OnCurve, CubicControlPoint1, CubicControlPoint2, QuadraticControlPoint };
 
@@ -53,6 +35,33 @@ struct Point {
         default: std::terminate();
         }
     }
+};
+
+/*! wide gamut sRGB color.
+ * Linear, with pre-multiplied alpha.
+ * Color component range 0-4095 is sRGB, beyond 0-4096 is allowed.
+ * Alpha component range 0-32767.
+ */
+struct Color {
+    big_int16_buf_t red;
+    big_int16_buf_t green;
+    big_int16_buf_t blue;
+    big_int16_buf_t alpha;
+};
+
+struct Contour {
+    Color fill;
+    Color stroke;
+    big_uint16_buf_t numberOfPoints;
+};
+
+struct Icon {
+    big_uint16_buf_t numberOfContours;
+};
+
+struct Header {
+    big_uint8_buf_t magic[4];
+    big_uint16_buf_t numberOfIcons;
 };
 
 
