a9a5c8f7b885fe73fa52a0e7c2df2973d86e2567
Take Vos <info@takevos.nl>
2020-05-31 10:26:09 +0200

    Add deflate file

---
+++ Foundation/include/TTauri/Foundation/deflate.hpp
@@ -0,0 +1,17 @@
+
+
+
+namespace TTauri {
+
+
+
+/** Decompress deflate data
+ *
+ *
+ */
+std::vector<std::byte> deflate_decompress(gsl::span<std::byte> compressed_buffer, size_t max_decompress_size)
+{
+    
+}
+
+}
