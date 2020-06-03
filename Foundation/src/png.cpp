a08354755952c8b4aeb0c45a850bfd30dade779d
Take Vos <info@takevos.nl>
2020-06-03 12:30:25 +0200

    More png implementation

--- Foundation/src/png.cpp
+++ Foundation/src/png.cpp
@@ -6,7 +6,57 @@
 
 namespace TTauri {
 
-struct PNG:wq
+struct ChunkHeader {
+    big_int32_buf_t length;    
+    big_int32_buf_t type;    
+};
+
+struct ChunkTrailer {
+    big_int32_buf_t crc32;    
+};
+
+struct IHDR {
+    ChunkHeader header;
+    big_int32_buf_t width;    
+    big_int32_buf_t height;    
+    uint8_t bit_depth;
+    uint8_t color_type;
+    uint8_t compression_method;
+    uint8_t filter_method;
+    uint8_t interlace_method;
+    ChunkTrailer trailer;
+};
+
+struct gAMA {
+    ChunkHeader header;
+    big_int32_buf_t gamma;    
+    ChunkTrailer trailer;
+};
+
+struct cHRM {
+    ChunkHeader header;
+    big_int32_buf_t white_point_x;    
+    big_int32_buf_t white_point_y;    
+    big_int32_buf_t red_x;    
+    big_int32_buf_t red_y;    
+    big_int32_buf_t green_x;    
+    big_int32_buf_t green_y;    
+    big_int32_buf_t blue_x;    
+    big_int32_buf_t blue_y;    
+    ChunkTrailer trailer;
+};
+
+struct sRGB {
+    ChunkHeader header;
+    uint8_t rendering_intent;
+    ChunkTrailer trailer;
+};
+
+struct PNGReadState {
+    placement_ptr<IHDR,std::byte const> ihdr;
+
+
+};
 
 std::pair<int,int> png_read_extent(nonstd::span<std::byte> bytes)
 {
