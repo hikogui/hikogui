d8e497f1824f33cd3ba2ee9f6f7e6ce165bb16d3
Take Vos <info@takevos.nl>
2020-08-06 10:49:13 +0200

    Add some comments

--- src/ttauri/thread.cpp
+++ src/ttauri/thread.cpp
@@ -13,16 +13,25 @@
 
 namespace tt {
 
+static std::atomic<uint32_t> thread_id_count = 0;
+
 void set_thread_name(std::string_view name)
 {
-    current_thread_id = thread_id_count.fetch_add(1, std::memory_order::memory_order_relaxed) + 1;
+    tt_assert(current_thread_id == 0);
 
 #if  TT_OPERATING_SYSTEM == TT_OS_WINDOWS
+    current_thread_id = GetCurrentThreadID();
+    tt_assert(current_thread_id != 0, "According to Microsoft no thread can have the value zero");
+
     ttlet wname = to_wstring(name);
     SetThreadDescription(GetCurrentThread(), wname.data());
 #elif  TT_OPERATING_SYSTEM == TT_OS_MACOS
+    current_thread_id = thread_id_count.fetch_add(1, std::memory_order::memory_order_relaxed) + 1;
+    tt_assert(current_thread_id != 0, "32 bit current_thread_id wrapped around");
     pthread_setname_np(name.data());
 #elif  TT_OPERATING_SYSTEM == TT_OS_LINUX
+    current_thread_id = thread_id_count.fetch_add(1, std::memory_order::memory_order_relaxed) + 1;
+    tt_assert(current_thread_id != 0, "32 bit current_thread_id wrapped around");
     pthread_setname_np(pthread_self(), name.data());
 #endif
 }
--- src/ttauri/thread.hpp
+++ src/ttauri/thread.hpp
@@ -11,14 +11,26 @@
 
 namespace tt {
 
+/** Set the name of the current thread.
+ * This function will set the name of the thread so that it is available
+ * by the operating system and debugger.
+ *
+ * Every thread should call this function exactly once.
+ *
+ * This function will also initialize the `current_thread_id`
+ */
 void set_thread_name(std::string_view name);
 
 bool is_main_thread();
 
 void run_on_main_thread(std::function<void()> f);
 
-inline std::atomic<uint32_t> thread_id_count = 0;
-
+/** The current_thread_id of a thread.
+ * This thread_id may not equal to the operating system's thread id.
+ * The current_thread_id should never be zero.
+ *
+ * The current_thread_id is initialized when calling `set_thread_name()`.
+ */
 inline thread_local uint32_t current_thread_id = 0;
 
 }
