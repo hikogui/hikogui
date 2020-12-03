f743d5b84615ac8d9230b0131eea73ac6028bb94
Take Vos <info@takevos.nl>
2020-12-03 15:10:26 +0100

    Add source location

---
+++ src/ttauri/source_location.hpp
@@ -0,0 +1,47 @@
+
+
+#pragma once
+
+namespace tt {
+
+class source_location {
+public:
+    constexpr source_location(uint_least32_t line, uint_least32_t column, char const *file_name, char const *function_name) noexcept :
+        _line(line), _column(column), _file_name(file_name), _function_name(function_name) {}
+
+    constexpr source_location(source_location const &) noexcept = default;
+    constexpr source_location(source_location &&) noexcept = default;
+    constexpr source_location &operator=(source_location const &) noexcept = default;
+    constexpr source_location &operator=(source_location &&) noexcept = default;
+
+    [[nodiscard]] constexpr uint_least32_t line() const noexcept
+    {
+        return _line;
+    }
+
+    [[nodiscard]] constexpr uint_least32_t column() const noexcept
+    {
+        return _column;
+    }
+
+    [[nodiscard]] constexpr char const *file_name() const noexcept
+    {
+        return _file_name;
+    }
+
+    [[nodiscard]] constexpr char const *function_name() const noexcept
+    {
+        return _function_name;
+    }
+
+private:
+    uint_least32_t _line;
+    uint_least32_t _column;
+    char const *file_name;
+    char const *function_name;
+};
+
+#define tt_source_location() tt::source_location(§
+
+}
+
