329b7c885d5997ecce7569af7fc6f6d285abad5a
Jens A. Koch <jakoch@web.de>
2021-02-20 18:39:30 +0100

    add helper to Display Compiler and Linker properties of Build Targets

---
+++ CMake/ShowBuildTargetProperties.cmake
@@ -0,0 +1,28 @@
+#
+# Show Build Target Properties
+#
+# Usage:
+# add_executable(my_exe WIN32 ${APP_SOURCES})
+# show_build_target_properties(my_exe)
+#
+
+function(show_build_target_property target property)
+  get_target_property(values ${target} ${property})
+  if(values)
+    if(NOT "${values}" STREQUAL "${property}-NOTFOUND")
+      message(STATUS "[${target}] ${property}: '${values}'")
+    endif()
+  endif()
+endfunction()
+
+function(show_build_target_properties target)
+  message(STATUS "== Properties of Build Target '${target}':")
+  message(" ")
+  show_build_target_property(${target} INCLUDE_DIRECTORIES)
+  show_build_target_property(${target} LINK_LIBRARIES)
+  show_build_target_property(${target} LINK_FLAGS)
+  show_build_target_property(${target} COMPILE_OPTIONS)
+  show_build_target_property(${target} COMPILE_DEFINITIONS)
+  show_build_target_property(${target} CMAKE_EXE_LINKER_FLAGS)
+  message(" ")
+endfunction()
