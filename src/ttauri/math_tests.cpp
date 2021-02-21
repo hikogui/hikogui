82fcf12022ad30767bb527e9b4c9ae29c0c26936
Take Vos <info@takevos.nl>
2021-02-21 19:18:03 +0100

    Fix #26 almost_equal unsigned integer mistake (#28)

--- src/ttauri/math_tests.cpp
+++ src/ttauri/math_tests.cpp
@@ -9,3 +9,77 @@
 using namespace std;
 using namespace tt;
 
+TEST(math, almost_equal)
+{
+    static_assert(almost_equal(0.0, 0.0) == true);
+    static_assert(almost_equal(0.0, 1.4012984643e-45f) == true);
+    static_assert(almost_equal(0.0, 6e-45f) == true);
+    static_assert(almost_equal(0.0, 7e-45f) == false);
+    static_assert(almost_equal(1.4012984643e-45f, 0.0) == true);
+    static_assert(almost_equal(6e-45f, 0.0) == true);
+    static_assert(almost_equal(7e-45f, 0.0) == false);
+
+    static_assert(almost_equal(-0.0, -0.0) == true);
+    static_assert(almost_equal(-0.0, -1.4012984643e-45f) == true);
+    static_assert(almost_equal(-0.0, -6e-45f) == true);
+    static_assert(almost_equal(-0.0, -7e-45f) == false);
+    static_assert(almost_equal(-1.4012984643e-45f, -0.0) == true);
+    static_assert(almost_equal(-6e-45f, -0.0) == true);
+    static_assert(almost_equal(-7e-45f, -0.0) == false);
+
+    static_assert(almost_equal(-0.0, 0.0) == true);
+    static_assert(almost_equal(-0.0, 1.4012984643e-45f) == true);
+    static_assert(almost_equal(-0.0, 6e-45f) == true);
+    static_assert(almost_equal(-0.0, 7e-45f) == false);
+    static_assert(almost_equal(-1.4012984643e-45f, 0.0) == true);
+    static_assert(almost_equal(-6e-45f, 0.0) == true);
+    static_assert(almost_equal(-7e-45f, 0.0) == false);
+
+    static_assert(almost_equal(0.0, -0.0) == true);
+    static_assert(almost_equal(0.0, -1.4012984643e-45f) == true);
+    static_assert(almost_equal(0.0, -6e-45f) == true);
+    static_assert(almost_equal(0.0, -7e-45f) == false);
+    static_assert(almost_equal(1.4012984643e-45f, -0.0) == true);
+    static_assert(almost_equal(6e-45f, -0.0) == true);
+    static_assert(almost_equal(7e-45f, -0.0) == false);
+
+    static_assert(almost_equal(1.4012984643e-45f, 1.4012984643e-45f) == true);
+    static_assert(almost_equal(1.4012984643e-45f, 7e-45f) == true);
+    static_assert(almost_equal(1.4012984643e-45f, 8e-45f) == false);
+
+    static_assert(almost_equal(-1.4012984643e-45f, -1.4012984643e-45f) == true);
+    static_assert(almost_equal(-1.4012984643e-45f, -7e-45f) == true);
+    static_assert(almost_equal(-1.4012984643e-45f, -8e-45f) == false);
+
+    static_assert(almost_equal(1.4012984643e-45f, -1.4012984643e-45f) == true);
+    static_assert(almost_equal(1.4012984643e-45f, -4e-45f) == true);
+    static_assert(almost_equal(1.4012984643e-45f, -5e-45f) == false);
+
+    static_assert(almost_equal(-1.4012984643e-45f, 1.4012984643e-45f) == true);
+    static_assert(almost_equal(-1.4012984643e-45f, 4e-45f) == true);
+    static_assert(almost_equal(-1.4012984643e-45f, 5e-45f) == false);
+
+    static_assert(almost_equal(1.0, 1.0) == true);
+    static_assert(almost_equal(1.0, 1.0000005f) == true);
+    static_assert(almost_equal(1.0, 1.0000006f) == false);
+    static_assert(almost_equal(1.0000005f, 1.0) == true);
+    static_assert(almost_equal(1.0000006f, 1.0) == false);
+
+    static_assert(almost_equal(-1.0, -1.0) == true);
+    static_assert(almost_equal(-1.0, -1.0000005f) == true);
+    static_assert(almost_equal(-1.0, -1.0000006f) == false);
+    static_assert(almost_equal(-1.0000005f, -1.0) == true);
+    static_assert(almost_equal(-1.0000006f, -1.0) == false);
+
+    static_assert(almost_equal(1.0, -1.0) == false);
+    static_assert(almost_equal(1.0, -1.0000005f) == false);
+    static_assert(almost_equal(1.0, -1.0000006f) == false);
+    static_assert(almost_equal(1.0000005f, -1.0) == false);
+    static_assert(almost_equal(1.0000006f, -1.0) == false);
+
+    static_assert(almost_equal(-1.0, 1.0) == false);
+    static_assert(almost_equal(-1.0, 1.0000005f) == false);
+    static_assert(almost_equal(-1.0, 1.0000006f) == false);
+    static_assert(almost_equal(-1.0000005f, 1.0) == false);
+    static_assert(almost_equal(-1.0000006f, 1.0) == false);
+}
