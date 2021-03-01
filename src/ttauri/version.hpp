f6547fbce8f49af7fa70d37c8cac387f4d877088
Take Vos <info@takevos.nl>
2021-03-01 16:22:49 +0100

    fix main

--- src/ttauri/version.hpp
+++ src/ttauri/version.hpp
@@ -20,28 +20,28 @@ struct version {
     std::string name = "ttauri";
 
     /** The version string with major.minor.patch, commits since tag, short hash of tag. */
-    std::string version_long = "0.2.3-18-g7443ac";
+    std::string version_long = "0.2.3-25-g7f6be3";
 
     /** A version string containing only major.minor.patch version numbers. */
     std::string version_short = "0.2.3";
 
     /* The git tag RCS. */
-    std::string git_tag_rcs = "v0.2.3-18-g7443aca0bec31b4c9c7f3b51da71f16365ec8e9e-dirty";
+    std::string git_tag_rcs = "v0.2.3-25-g7f6be3fa7a10d204bb09ffc20b7fd024e536eb59-dirty";
 
     /** Name of the git branch. */
-    std::string git_branch = "relative-clipping-rectangle";
+    std::string git_branch = "main";
 
     /** The git commit short hash. */
-    std::string git_commit = "7443aca";
+    std::string git_commit = "7f6be3f";
 
     /** The number of commits since the version tag. */
-    int git_commits_since_version_tag = 18;
+    int git_commits_since_version_tag = 25;
 
     /** There are local changes. */
     bool git_local_changes = "dirty";
 
     /** The release date shows when the release was tagged. */
-    std::string release_date = "2021-02-27";
+    std::string release_date = "2021-03-01";
 
 };
 
