816f9791a26427df0ec2d91931d7acaf85123da0
Jhalak Patel <jhalakpatel@outlook.com>
2021-04-12 10:16:37 -0700

    Update log message with tid and cpu fields
    Co-authored-by: Take Vos <info@takevos.nl>

--- src/ttauri/logger.hpp
+++ src/ttauri/logger.hpp
@@ -67,7 +67,7 @@ public:
         ttlet cpu_id = _time_stamp.cpu_id();
 
         if constexpr (static_cast<bool>(Level & log_level::statistics)) {
-            return fmt::format("{} {:5} {} {} {}\n", local_timestring, to_const_string(Level), _what(), _thread_id, cpu_id);
+            return fmt::format("{} {:5} {} tid={} cpu={}\n", local_timestring, to_const_string(Level), _what(), _thread_id, cpu_id);
         } else {
             return fmt::format("{} {:5} {} ({}:{}) {} {}\n", local_timestring, to_const_string(Level), _what(), SourceFile, SourceLine, _thread_id, cpu_id);
         }
