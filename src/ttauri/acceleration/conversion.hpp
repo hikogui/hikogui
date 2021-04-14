2e98e8938a45947a971ce6f6b63754d27c6d30d3
Take Vos <info@takevos.nl>
2021-04-14 14:01:15 +0200

    Make load samples for floats and ints

--- src/ttauri/acceleration/conversion.hpp
+++ src/ttauri/acceleration/conversion.hpp
@@ -1,5 +1,13 @@
+// Copyright Take Vos 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
 
 #include <cstdint>
+#include <bit>
+#include <memory>
+#include "../geometry/numeric_array.hpp"
 
 namespace tt {
 
@@ -18,191 +26,173 @@ inline float hmax(__m256 x)
     peak_r = _mm_permute_ps(peak_l, 0b00'00'00'01);
     peak_l = _mm_max_ps(peak_l, peak_r);
     float peak_s = _mm_cvtss_f32(peak_l, 0);
-
 }
-
 
 struct convert_result {
     float peak;
     float mean;
 };
 
-/** Convert 32 bit integer to 32 bit float.
- * @param x A vector of 32 bit integers to convert.
- * @param mul The multiplier used for the conversion of integer to float
- * @param [in,out]peak The continues peak value.
- * @param [in,out]sum The continues sum value.
- * @return A vector of 32 bit floating point numbers.
+struct load_samples_context {
+    /** Multiplier to use to convert integers into normalized floating point numbers between -1.0 and 1.0.
+     */
+    f32x8 multiplier;
+
+    /** Permute mask used to extract, extend & endian-swap short integers into int32_t.
+     */
+    i8x16 permute_mask;
+
+    /** How much to advance the input pointer after each 128-bit load.
+     */
+    size_t load_stride;
+
+    /** How many samples are loaded with each 128-bit load.
+     */
+    size_t samples_per_load;
+
+    [[nodiscard]] static constexpr i8x16
+    make_permute_mask(size_t num_bytes_per_sample, std::endian endian, size_t samples_per_load, size_t stride)
+    {
+        i8x16 r;
+
+        for (size_t dst_i = 0; dst_i != 16; ++dst_i) {
+            // clang-format off
+            ttlet sample_index = dst_i / 4;
+
+            // The byte index inside the sample counting from the least-significant byte.
+            ttlet sample_byte_index = (std::endian::native == std::endian::little) ?
+                dst_i % 4 :
+                3 - (dst_i % 4);
+
+            if (sample_index < samples_per_load && sample_byte_index < num_bytes_per_sample) {
+                ttlet src_i = (endian == std::endian::little) ? 
+                    sample_index * stride + sample_byte_index :
+                    sample_index * stride + (num_bytes_per_sample - 1 - sample_byte_index);
+
+                permute_mask[dst_i] = narrow_cast<int8_t>(src_i);
+
+            } else {
+                // Any other byte is set to zero.
+                permute_mask[dst_i] = -1;
+            }
+            // clang-format on
+        }
+
+        return r;
+    }
+
+    [[nodiscard]] static constexpr calculate_samples_per_load(size_t num_bytes_per_sample, size_t stride) noexcept
+    {
+        // Determine the amount of samples that can be loaded in a single 128-bit load.
+        if ((4 * stride + num_bytes_per_sample) <= 16) {
+            return 4;
+        } else if ((2 * stride + num_bytes_per_sample) <= 16) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    /** Initialize a context for loading signed integer samples.
+     * @param num_bytes_per_sample The number of byte that is used to represent an audio sample.
+     * @param num_bit_per_sample The number of significant bits of the audio sample.
+     * @param sample_is_normalized If true the significant bits of the audio sample is scaled up
+     *        to fit inside the number of bytes available.
+     * @param endian The byte order of the sample in memory.
+     * @param stride The number of bytes to increment a pointer to the next sample.
+     */
+    [[nodiscard]] static constexpr load_samples_context int_samples(
+        size_t num_bytes_per_sample,
+        size_t num_bits_per_sample,
+        bool sample_is_normalized,
+        std::endian endian,
+        size_t stride) noexcept
+    {
+        // The multiplier is based on the actual number of bits in the sample
+        auto max_sample_value = (1_uz << (num_bits_per_sample - 1)) - 1;
+        if (sample_is_normalized) {
+            max_sample_value <<= (num_bytes_per_sample * 8 - num_bits);
+        }
+
+        multiplier = f32x8::broadcast(1.0f / static_cast<float>(max_sample_value));
+
+        samples_per_load = calculate_samples_per_load(num_bytes_per_samples, stride);
+
+        load_stride = stride * samples_per_load;
+
+        permute_mask = make_permute_mask(num_bytes_per_sample, endian, samples_per_load, stride);
+    }
+
+    [[nodiscard]] constexpr load_samples_context float_samples(size_t stride) noexcept
+    {
+        multiplier = f32x8::broadcast(1.0f);
+
+        samples_per_load = calculate_samples_per_load(sizeof(float), stride);
+
+        load_stride = stride * samples_per_load;
+
+        permute_mask = make_permute_mask(sizeof(float), std::endian::native, samples_per_load, stride);
+    }
+
+    [[nodiscard]] constexpr bool is_valid() const noexcept
+    {
+        return load_stride != 0 && (samples_per_load == 1 || samples_per_load == 2 || samples_per_load == 4);
+    }
+};
+
+/** Load 4 signed integer samples from memory.
+ * @param [in,out]ptr The pointer to the first sample of four samples.
+ * @param context The context to use to extract the sample from memory.
+ * @return 4 signed integer samples.
  */
-inline float sample_i32_to_f32(int32_t x, float mul, float &peak, float &sum) noexcept
+[[nodiscard]] constexpr i32x4 load_4_samples(int8_t const *&ptr, load_samples_context const &context) noexcept
 {
-    ttlet x_f32 = static_cast<float>(x);
-    ttlet x_f32_mul = x * mul;
+    tt_axiom(context.is_valid());
+    tt_axiom(ptr != nullptr);
 
-    ttlet x_f32_mul_abs = std::abs(x_f32_mul);
-    peak = std::max(peak, x_f32_mul_abs);
-    sum += x_f32_mul_abs;
-    return x_f32_mul;
-}
+    auto r = i8x16{};
 
-/** Convert 8x 32 bit integer to 32 bit float.
- * @param x A vector of 32 bit integers to convert.
- * @param mul The multiplier used for the conversion of integer to float
- * @param [in,out]peak The continues peak value.
- * @param [in,out]sum The continues sum value.
- * @return A vector of 32 bit floating point numbers.
- */
-inline __m256 sample_i32_to_f32(__m256i x, __m256 mul, __m256 &peak, __m256 &sum) noexcept
-{
-    ttlet x_f32 = _mm256_cvtepi32_ps(x);
-    ttlet x_f32_mul = _mm256_mul_ps(f32_8, mul);
-
-    // Get the peak and average.
-    ttlet x_f32_mul_abs = _mm256_abs_ps(x_f32_mul);
-    peak = _mm256_max_ps(peak, x_f32_mul_abs);
-    sum = _mm256_add_ps(sum, x_f32_mul_abs);
-
-    return x_f32_mul;
-}
-
-/** Convert 8x 16 bit integer to 32 bit float.
- * The 16 bit integers are first converted to 32 bit integers by copying
- * by repeating the 16 bits.
- *
- * @param x A vector of 16 bit integers to convert.
- * @param mul The multiplier used for the conversion of integer to float
- * @param [in,out]peak The continues peak value.
- * @param [in,out]sum The continues sum value.
- * @return A vector of 32 bit floating point numbers.
- */
-inline __m256 sample_i16_to_f32(__m128i x, __m256 mul, __m256 &peak, __m256 &sum) noexcept
-{
-    ttlet x_db_lo = _mm_unpacklo_epi16(x, x);
-    ttlet x_db_hi = _mm_unpackhi_epi16(x, x);
-    ttlet x_db = _mm256_set_m128i(x_db_hi, x_db_lo);
-    return sample_i32_to_f32(x_db, mul, peak, sum);
-}
-
-[[nodiscard]] constexpr i32x4 load_4x_sint(int8_t const *&in_ptr, size_t load_stride, size_t ints_per_load, i8x16 permute_mask) noexcept
-{
-    auto r = i32x4{};
-
-    for (size_t i = 0; i != 4; i += ints_per_load) {
-        auto x = i8x16(in_ptr);
-        in_ptr += load_stride;
+    for (size_t i = 0; i != 4; i += context.samples_per_load) {
+        ttlet packed_samples = i8x16(ptr);
+        ptr += context.load_stride;
 
         // The permute mask will make 32 bit integers, including endian change.
         // Unused integers are set to zero.
-        auto x_unpacked = shuffle(x, permute_mask);
-        auto x_casted = std::bit_cast<i32x4>(x_unpacked);
+        ttlet i32_samples = shuffle(packed_samples, context.permute_mask);
 
         // Insert the integers into r0
-        r0 |= byte_shift_left(x_casted, i * 4);
+        r |= byte_shift_left(i32_samples, i * 4);
     }
 
-    return r;
+    return std::bit_cast<i32x4>(r);
 }
 
-/** Load 8x n-bit signed integers from memory.
- * After loading an integer it is scaled up to an int32_t. The small integer is scaled up
- * by concatonating repeatedly the bits of the small integer starting with the most significant bits.
- *
- * loads are done using _mm_loadu_si128, since intel CPUs will do reads at 128 bits at a time.
- * And since the bytes needs to be permuted we don't use larger loads because of the inability to
- * cross the 128 bits lanes.
- *
- * We may do overlapping unaligned loads, this makes for simpler loads which require only a single
- * permute mask.
- *
- * @param in_ptr A pointer to unaligned n-bit integers.
- * @param nr_bits The number of bits of each integer, does not need to be multiple of bytes.
- * @return 8x int32_t scaled up from the input integers
+/** Load 8 signed integer samples from memory.
+ * @param [in,out]ptr The pointer to the first sample of eight samples.
+ * @param context The context to use to extract the sample from memory.
+ * @return 8 signed integer samples.
  */
-i32x8 load_8x_sint(int8_t const *&in_ptr, size_t load_stride, size_t ints_per_load, i8x16 permute_mask)
+[[nodiscard]] constexpr i32x8 load_8_samples(int8_t const *&ptr, load_samples_context const &context)
 {
-    auto r0 = load_4_sint(in_ptr, load_stride, ints_per_load, permute_mask);
-    auto r1 = load_4_sint(in_ptr, load_stride, ints_per_load, permute_mask);
+    ttlet r0 = load_4_samples(ptr, context);
+    ttlet r1 = load_4_samples(ptr, context);
     return {r0, r1};
 }
 
-
-/** Convert 16 bit integer interleaved samples to a 32 bit float buffer.
- * This function uses streaming SSE/AVX instructions which will bypass
- * the cache. It is expected that 
- * 
+/** Load 8 signed integer samples from memory.
+ * @param [in,out]ptr The pointer to the first sample of eight samples.
+ * @param context The context to use to extract the sample from memory.
+ * @return 8 normalized floating point samples.
  */
-convert_result convert_i16_to_f32(
-    char const * restrict in_ptr,
-    char const * restrict in_last,
-    size_t in_stride,
-    float *restrict out_ptr) noexcept
+[[nodiscard]] constexpr f32x8 load_8_int_samples(int8_t const *&ptr, load_samples_context const &context)
 {
-    auto in_stride_v = in_stride * 8;
-    auto in_last_v = floor(in_last, in_stride_v);
-
-    auto mul = 1.0f / 2147483647.0f;
-    auto peak = 0.0f;
-    auto sum = 0.0f;
-
-    if (stride == 2) {
-        auto mul_v = _mm256_set1_ps(sum);
-        auto peak_v = _mm256_set_zero_ps();
-        auto sum_v = _mm256_set_zero_ps();
-        while (in_ptr != in_last_v) {
-            auto i16_v = _mm_loadu_si128(in_ptr);
-            in_ptr += in_stride_v;
-            auto f32_v = sample_i16_to_f32(i16_v, mul_v, peak_v, sum_v);
-            _mm_storeu_ps(f32_v, out_ptr);
-            out_ptr += 8;
-        }
-        peak = hmax(peak_v);
-        sum = hadd(sum_v);
-
-     } else if (stride == 4) {
-        auto mul_v = _mm256_set1_ps(sum);
-        auto peak_v = _mm256_set_zero_ps();
-        auto sum_v = _mm256_set_zero_ps();
-        auto mask_v = _mm_set_epi8(128,128, 128,128, 128,128, 128,128, 13,12, 9,8  5,4, 1,0)
-        while (in_ptr != in_last_v) {
-            auto i16_v = _mm256_loadu_si256(in_ptr);
-            in_ptr += in_stride_v;
-            auto f32_v = sample_i16_to_f32_stride2(i16_v, mul_v, mask_v, peak_v, sum_v);
-            _mm_storeu_ps(f32_v, out_ptr);
-            out_ptr += 8;
-        }
-        peak = hmax(peak_v);
-        sum = hadd(sum_v);
-
-     } else {
-        auto mul_v = _mm256_set1_ps(sum);
-        auto peak_v = _mm256_set_zero_ps();
-        auto sum_v = _mm256_set_zero_ps();
-        while (in_ptr != in_last_v) {
-            auto i16_v = _mm_set_zero_ps();
-            for (int i = 0; i != 8; ++i) {
-                int16_t i16;
-                std::memcpy(&i16, in_ptr, 2);
-                in_ptr += stride;
-                i16_v = _mm_insert_epi16(i16_v, i16, i);
-            }
-
-            auto f32_v = sample_i16_to_f32(i16_v, mul_v, peak_v, sum_v);
-            _mm_storeu_ps(f32_v, out_ptr);
-            out_ptr += 8;
-        }
-        peak = hmax(peak_v);
-        sum = hadd(sum_v);
-     }
-
-     while (in_ptr != in_last) {
-        int16_t i16;
-        std::memcpy(&i16, in_ptr, 2);
-        *out_ptr = sample_i16_to_f32(i16, mul, peak, sum);
-        in_ptr += stride;
-        ++out_ptr;
-     }
+    ttlet int_samples = f32x8{load_8_samples(int8_t const *&ptr, load_samples_context const &context)};
+    return f32x8{int_samples} * context.multiplier;
 }
 
-
+[[nodiscard]] constexpr f32x8 load_8_float_samples(int8_t const *&ptr, load_float_samples_context const &context)
+{
+    return std::bit_cast<f32x8>(load_8_samples(int8_t const *&ptr, load_samples_context const &context));
 }
 
+} // namespace tt
