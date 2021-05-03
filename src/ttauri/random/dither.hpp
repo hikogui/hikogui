94abe1a0ca4d4cf6b9f8592629c11ce618c4e8d5
Take Vos <info@takevos.nl>
2021-05-03 13:27:16 +0200

    Add unit-test for dither

--- src/ttauri/random/dither.hpp
+++ src/ttauri/random/dither.hpp
@@ -17,14 +17,21 @@ public:
     dither &operator=(dither &&) = default;
 
     /** Create a dither object.
-     * 
+     *
      * @param num_bits Number of significant fraction bits, excluding the sign bit.
      *                 For 24 bit signed PCM samples this value is 23.
      */
     dither(int num_bits) noexcept : _state(), _multiplier()
     {
         tt_axiom(num_bits > 0);
-        auto maximum_value = static_cast<float>((1_uz << num_bits) - 1) * 2147483647.0f;
+        auto maximum_value = static_cast<float>((1_uz << num_bits) - 1);
+
+        // The maximum value from the rectangular probability density function.
+        maximum_value *= 2147483647.0f;
+
+        // Triangular probability density function is has twice the range.
+        maximum_value *= 2.0f;
+
         _multiplier = f32x4::broadcast(1.0f / maximum_value);
     }
 
@@ -33,7 +40,10 @@ public:
      */
     [[nodiscard]] f32x4 next() noexcept
     {
-        return f32x4{_state.next<i32x4>()} + f32x4{_state.next<i32x4>()} * _multiplier;
+        auto rpdf1 = f32x4{_state.next<i32x4>()};
+        auto rpdf2 = f32x4{_state.next<i32x4>()};
+        auto tpdf = rpdf1 + rpdf2;
+        return tpdf * _multiplier;
     }
 
 private:
@@ -41,4 +51,4 @@ private:
     f32x4 _multiplier;
 };
 
-}
+} // namespace tt
---
+++ src/ttauri/random/dither_tests.cpp
@@ -0,0 +1,95 @@
+// Copyright Take Vos 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#include "dither.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <map>
+#include <cmath>
+
+using namespace std;
+using namespace tt;
+
+[[nodiscard]] static std::map<int,float> dither_test(int num_bits, float sample_value) noexcept
+{
+    auto sample_count = 100000;
+    auto sample_percentage = 100.0f / sample_count;
+
+    // Signed 16 bit PCM has 15 bits of fraction.
+    auto d = dither(num_bits);
+
+    // The maximum value of a 16 bit PCM sample.
+    auto max_sample_value = static_cast<float>((1_uz << num_bits) - 1);
+    auto rcp_max_sample_value = 1.0f / max_sample_value;
+    auto scaled_sample_value = sample_value * rcp_max_sample_value;
+
+    auto results = std::map<int, float>{};
+    for (auto i = 0; i != sample_count; ++i) {
+        auto dithered_sample_value = scaled_sample_value + d.next()[0];
+
+        auto result_sample_value = static_cast<int>(std::round(dithered_sample_value * max_sample_value));
+
+        auto it = results.find(result_sample_value);
+        if (it == results.end()) {
+            results[result_sample_value] = 1.0f * sample_percentage;
+        } else {
+            it->second += 1.0f * sample_percentage;
+        }
+    }
+
+    return results;
+}
+
+TEST(dither, PCM8_1_0)
+{
+    auto results = dither_test(7, 1.0f);
+
+    ASSERT_TRUE(results[0] > 11.5f and results[0] < 13.5f);
+    ASSERT_TRUE(results[1] > 74.0f and results[1] < 76.0f);
+    ASSERT_TRUE(results[2] > 11.5f and results[2] < 13.5f);
+}
+
+TEST(dither, PCM16_1_0)
+{
+    auto results = dither_test(15, 1.0f);
+
+    ASSERT_TRUE(results[0] > 11.5f and results[0] < 13.5f);
+    ASSERT_TRUE(results[1] > 74.0f and results[1] < 76.0f);
+    ASSERT_TRUE(results[2] > 11.5f and results[2] < 13.5f);
+}
+
+TEST(dither, PCM24_1_0)
+{
+    auto results = dither_test(23, 1.0f);
+
+    ASSERT_TRUE(results[0] > 11.5f and results[0] < 13.5f);
+    ASSERT_TRUE(results[1] > 74.0f and results[1] < 76.0f);
+    ASSERT_TRUE(results[2] > 11.5f and results[2] < 13.5f);
+}
+
+TEST(dither, PCM8_1_5)
+{
+    auto results = dither_test(7, 1.5f);
+
+    ASSERT_TRUE(results[1] > 49.0f and results[1] < 51.0f);
+    ASSERT_TRUE(results[2] > 49.0f and results[2] < 51.0f);
+}
+
+TEST(dither, PCM16_1_5)
+{
+    auto results = dither_test(15, 1.5f);
+
+    ASSERT_TRUE(results[1] > 49.0f and results[1] < 51.0f);
+    ASSERT_TRUE(results[2] > 49.0f and results[2] < 51.0f);
+}
+
+TEST(dither, PCM24_1_5)
+{
+    auto results = dither_test(23, 1.5f);
+
+    ASSERT_TRUE(results[1] > 49.0f and results[1] < 51.0f);
+    ASSERT_TRUE(results[2] > 49.0f and results[2] < 51.0f);
+}
