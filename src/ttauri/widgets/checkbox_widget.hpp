556f3102735dfbfeb3948d57651efc8b1b5cf8c4
Take Vos <info@takevos.nl>
2021-07-05 17:14:51 +0200

    Documentation for checkbox widget.

--- src/ttauri/widgets/checkbox_widget.hpp
+++ src/ttauri/widgets/checkbox_widget.hpp
@@ -9,18 +9,31 @@
 
 namespace tt {
 
+/** A checkbox widget.
+ */
 class checkbox_widget final : public abstract_button_widget {
 public:
     using super = abstract_button_widget;
     using delegate_type = typename super::delegate_type;
     using callback_ptr_type = typename delegate_type::callback_ptr_type;
 
-    checkbox_widget(gui_window &window, widget *parent, weak_or_unique_ptr<delegate_type> delegate) noexcept :
-        super(window, parent, std::move(delegate))
-    {
-        label_alignment = alignment::top_left;
-    }
+    /** Construct a checkbox widget.
+     *
+     * @param window The window that this widget belongs to.
+     * @param parent The parent widget that owns this checkbox widget.
+     * @param delegate The delegate to use to manage the state of the checkbox button.
+     */
+    checkbox_widget(gui_window &window, widget *parent, std::weak_ptr<delegate_type> delegate) noexcept;
 
+    /** Construct a checkbox widget with a default button delegate.
+     *
+     * @see default_button_delegate
+     * @param window The window that this widget belongs to.
+     * @param parent The parent widget that owns this checkbox widget.
+     * @param value The value or `observable` value which represents the state of the checkbox.
+     * @param args An optional on-value, followed by an optional off-value. These two values
+     *             are used to determine which value yields an on/off state.
+     */
     template<typename Value, typename... Args>
     requires(not std::is_convertible_v<Value, weak_or_unique_ptr<delegate_type>>)
         checkbox_widget(gui_window &window, widget *parent, Value &&value, Args &&...args) noexcept :
@@ -31,64 +44,11 @@ public:
     {
     }
 
-    [[nodiscard]] bool constrain(hires_utc_clock::time_point display_time_point, bool need_reconstrain) noexcept override
-    {
-        tt_axiom(is_gui_thread());
-
-        if (super::constrain(display_time_point, need_reconstrain)) {
-            // Make room for button and margin.
-            _button_size = {theme::global().size, theme::global().size};
-            ttlet extra_size = extent2{theme::global().margin + _button_size.width(), 0.0f};
-            _minimum_size += extra_size;
-            _preferred_size += extra_size;
-            _maximum_size += extra_size;
-
-            _minimum_size = max(_minimum_size, _button_size);
-            _preferred_size = max(_minimum_size, _button_size);
-            _maximum_size = max(_minimum_size, _button_size);
-
-            tt_axiom(_minimum_size <= _preferred_size && _preferred_size <= _maximum_size);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    [[nodiscard]] void layout(hires_utc_clock::time_point displayTimePoint, bool need_layout) noexcept override
-    {
-        tt_axiom(is_gui_thread());
-
-        need_layout |= _request_layout.exchange(false);
-        if (need_layout) {
-            _button_rectangle = align(rectangle(), _button_size, alignment::top_left);
-
-            _label_rectangle = aarectangle{_button_rectangle.right() + theme::global().margin, 0.0f, width(), height()};
-
-            _check_glyph = to_font_glyph_ids(elusive_icon::Ok);
-            ttlet check_glyph_bb = pipeline_SDF::device_shared::getBoundingBox(_check_glyph);
-            _check_glyph_rectangle =
-                align(_button_rectangle, scale(check_glyph_bb, theme::global().icon_size), alignment::middle_center);
-
-            _minus_glyph = to_font_glyph_ids(elusive_icon::Minus);
-            ttlet minus_glyph_bb = pipeline_SDF::device_shared::getBoundingBox(_minus_glyph);
-            _minus_glyph_rectangle =
-                align(_button_rectangle, scale(minus_glyph_bb, theme::global().icon_size), alignment::middle_center);
-        }
-        super::layout(displayTimePoint, need_layout);
-    }
-
-    void draw(draw_context context, hires_utc_clock::time_point display_time_point) noexcept override
-    {
-        tt_axiom(is_gui_thread());
-
-        if (overlaps(context, _clipping_rectangle)) {
-            draw_check_box(context);
-            draw_check_mark(context);
-        }
-
-        super::draw(std::move(context), display_time_point);
-    }
-
+    /// @privatesection
+    [[nodiscard]] bool constrain(hires_utc_clock::time_point display_time_point, bool need_reconstrain) noexcept override;
+    [[nodiscard]] void layout(hires_utc_clock::time_point displayTimePoint, bool need_layout) noexcept override;
+    void draw(draw_context context, hires_utc_clock::time_point display_time_point) noexcept override;
+    /// @endprivatesection
 private:
     extent2 _button_size;
     aarectangle _button_rectangle;
@@ -97,29 +57,10 @@ private:
     font_glyph_ids _minus_glyph;
     aarectangle _minus_glyph_rectangle;
 
-    void draw_check_box(draw_context const &context) noexcept
-    {
-        tt_axiom(is_gui_thread());
-        context.draw_box_with_border_inside(_button_rectangle, background_color(), focus_color());
-    }
+    checkbox_widget(gui_window &window, widget *parent, weak_or_unique_ptr<delegate_type> delegate) noexcept;
 
-    void draw_check_mark(draw_context context) noexcept
-    {
-        tt_axiom(is_gui_thread());
-
-        auto state_ = state();
-
-        // Checkmark or tristate.
-        if (state_ == tt::button_state::on) {
-            context.draw_glyph(_check_glyph, translate_z(0.1f) * _check_glyph_rectangle, accent_color());
-
-        } else if (state_ == tt::button_state::off) {
-            ;
-
-        } else {
-            context.draw_glyph(_minus_glyph, translate_z(0.1f) * _minus_glyph_rectangle, accent_color());
-        }
-    }
+    void draw_check_box(draw_context const &context) noexcept;
+    void draw_check_mark(draw_context context) noexcept;
 };
 
 } // namespace tt
--- src/ttauri/widgets/default_button_delegate.hpp
+++ src/ttauri/widgets/default_button_delegate.hpp
@@ -12,27 +12,44 @@
 
 namespace tt {
 
+/** A default button delegate.
+ *
+ * The default button delegate manages the state of a button widget using
+ * observable values.
+ *
+ * @tparam ButtonType The type of button this delegate manages, either a
+ *         `button_type::radio` or `button_type::toggle`.
+ * @tparam T The type of the observable value.
+ */
 template<button_type ButtonType, typename T>
 class default_button_delegate : public button_delegate {
 public:
     static_assert(ButtonType == button_type::radio or ButtonType == button_type::toggle);
 
     using value_type = T;
+    static constexpr button_type button_type = ButtonType;
     static constexpr bool can_make_defaults =
         std::is_same_v<value_type, bool> or std::is_integral_v<value_type> or std::is_enum_v<value_type>;
 
-    static constexpr button_type button_type = ButtonType;
-
-    observable<value_type> value;
-    observable<value_type> on_value;
-    observable<value_type> off_value;
-
+    /** Construct a delegate.
+     *
+     * @param value A value or observable-value used as a representation of the state.
+     * @param on_value The value or observable-value that mean 'on'.
+     * @param off_value The value or observable-value that mean 'off'.
+     */
     template<typename Value, typename OnValue, typename OffValue>
     default_button_delegate(Value &&value, OnValue &&on_value, OffValue &&off_value) noexcept :
-        value(std::forward<Value>(value)), on_value(std::forward<OnValue>(on_value)), off_value(std::forward<OnValue>(off_value))
+        _value(std::forward<Value>(value)),
+        _on_value(std::forward<OnValue>(on_value)),
+        _off_value(std::forward<OnValue>(off_value))
     {
     }
 
+    /** Construct a delegate.
+     *
+     * @param value A value or observable-value used as a representation of the state.
+     * @param on_value The value or observable-value that mean 'on'.
+     */
     template<typename Value, typename OnValue>
     default_button_delegate(Value &&value, OnValue &&on_value) noexcept
         requires(can_make_defaults or button_type == button_type::radio) :
@@ -40,37 +57,37 @@ public:
     {
     }
 
+    /** Construct a delegate.
+     *
+     * @param value A value or observable-value used as a representation of the state.
+     */
     template<typename Value>
     default_button_delegate(Value &&value) noexcept requires(can_make_defaults) :
         default_button_delegate(std::forward<Value>(value), static_cast<value_type>(1), value_type{})
     {
     }
 
-    default_button_delegate() noexcept requires(can_make_defaults) :
-        default_button_delegate(value_type{}, static_cast<value_type>(1), value_type{})
-    {
-    }
-
+    /// @privatesection
     callback_ptr_type subscribe(abstract_button_widget &sender, callback_ptr_type const &callback_ptr) noexcept override
     {
-        value.subscribe(callback_ptr);
-        on_value.subscribe(callback_ptr);
-        off_value.subscribe(callback_ptr);
+        _value.subscribe(callback_ptr);
+        _on_value.subscribe(callback_ptr);
+        _off_value.subscribe(callback_ptr);
         return callback_ptr;
     }
 
     void unsubscribe(abstract_button_widget &sender, callback_ptr_type const &callback_ptr) noexcept override
     {
-        value.unsubscribe(callback_ptr);
-        on_value.unsubscribe(callback_ptr);
-        off_value.unsubscribe(callback_ptr);
+        _value.unsubscribe(callback_ptr);
+        _on_value.unsubscribe(callback_ptr);
+        _off_value.unsubscribe(callback_ptr);
     }
 
     [[nodiscard]] button_state state(abstract_button_widget const &sender) const noexcept override
     {
-        if (value == on_value) {
+        if (_value == _on_value) {
             return button_state::on;
-        } else if (value == off_value) {
+        } else if (_value == _off_value) {
             return button_state::off;
         } else {
             return button_state::other;
@@ -80,15 +97,20 @@ public:
     void activate(abstract_button_widget &sender) noexcept override
     {
         if constexpr (button_type == button_type::toggle) {
-            if (value == off_value) {
-                value = *on_value;
+            if (_value == _off_value) {
+                _value = *_on_value;
             } else {
-                value = *off_value;
+                _value = *_off_value;
             }
         } else if constexpr (button_type == button_type::radio) {
-            value = *on_value;
+            _value = *_on_value;
         }
     }
+    /// @endprivatesection
+private:
+    observable<value_type> _value;
+    observable<value_type> _on_value;
+    observable<value_type> _off_value;
 };
 
 template<button_type ButtonType, typename Value, typename... Args>
