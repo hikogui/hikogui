83adbc15ab7f6b6476cfeebe3af81079dd942243
Take Vos <info@takevos.nl>
2021-10-29 21:30:24 +0200

    change corner shapes to uint8 (#260)
    * change corner shapes to uint8

--- src/ttauri/GFX/pipeline_box_vertex.hpp
+++ src/ttauri/GFX/pipeline_box_vertex.hpp
@@ -9,14 +9,15 @@
 #include "../rapid/sfloat_rgba16.hpp"
 #include "../rapid/sfloat_rgba32.hpp"
 #include "../rapid/sfloat_rgb32.hpp"
+#include "../rapid/uint_abgr8_pack.hpp"
 #include <vulkan/vulkan.hpp>
 #include <span>
 
 namespace tt::pipeline_box {
 
 /*! A vertex defining a rectangle on a window.
-* The vertex shader will convert window pixel-coordinates to normalized projection-coordinates.
-*/
+ * The vertex shader will convert window pixel-coordinates to normalized projection-coordinates.
+ */
 struct vertex {
     //! The pixel-coordinates where the origin is located relative to the bottom-left corner of the window.
     sfloat_rgb32 position;
@@ -24,12 +25,10 @@ struct vertex {
     //! The position in pixels of the clipping rectangle relative to the bottom-left corner of the window, and extent in pixels.
     sfloat_rgba32 clipping_rectangle;
 
-    /** Double 2D coordinates inside the quad, used to determine the distance from the sides and corner inside the fragment shader.
-     * x = Number of pixels to the right from the left edge of the quad.
-     * y = Number of pixels above the bottom edge.
-     * z = Number of pixels to the left from the right edge of the quad.
-     * w = Number of pixels below the top edge.
-     * 
+    /** Double 2D coordinates inside the quad, used to determine the distance from the sides and corner inside the fragment
+     * shader. x = Number of pixels to the right from the left edge of the quad. y = Number of pixels above the bottom edge. z =
+     * Number of pixels to the left from the right edge of the quad. w = Number of pixels below the top edge.
+     *
      * The rasteriser will interpolate these numbers, so that inside the fragment shader
      * the distance from a corner can be determined easily.
      */
@@ -42,7 +41,7 @@ struct vertex {
     sfloat_rgba16 line_color;
 
     //! Shape of each corner, negative values are cut corners, positive values are rounded corners.
-    sfloat_rgba16 corner_shapes;
+    uint_abgr8_pack corner_shapes;
 
     float line_width;
 
@@ -53,8 +52,7 @@ struct vertex {
         color fill_color,
         color line_color,
         float line_width,
-        tt::corner_shapes corner_shapes
-    ) noexcept :
+        tt::corner_shapes corner_shapes) noexcept :
         position(position),
         clipping_rectangle(clipping_rectangle),
         corner_coordinate(corner_coordinate),
@@ -67,22 +65,20 @@ struct vertex {
 
     static vk::VertexInputBindingDescription inputBindingDescription()
     {
-        return {
-            0, sizeof(vertex), vk::VertexInputRate::eVertex
-        };
+        return {0, sizeof(vertex), vk::VertexInputRate::eVertex};
     }
 
     static std::vector<vk::VertexInputAttributeDescription> inputAttributeDescriptions()
     {
         return {
-            { 0, 0, vk::Format::eR32G32B32Sfloat, offsetof(vertex, position) },
-            { 1, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(vertex, clipping_rectangle) },
-            { 2, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(vertex, corner_coordinate) },
-            { 3, 0, vk::Format::eR16G16B16A16Sfloat, offsetof(vertex, fill_color) },
-            { 4, 0, vk::Format::eR16G16B16A16Sfloat, offsetof(vertex, line_color) },
-            { 5, 0, vk::Format::eR16G16B16A16Sfloat, offsetof(vertex, corner_shapes) },
-            { 6, 0, vk::Format::eR32Sfloat, offsetof(vertex, line_width) },
+            {0, 0, vk::Format::eR32G32B32Sfloat, offsetof(vertex, position)},
+            {1, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(vertex, clipping_rectangle)},
+            {2, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(vertex, corner_coordinate)},
+            {3, 0, vk::Format::eR16G16B16A16Sfloat, offsetof(vertex, fill_color)},
+            {4, 0, vk::Format::eR16G16B16A16Sfloat, offsetof(vertex, line_color)},
+            {5, 0, vk::Format::eA8B8G8R8UintPack32, offsetof(vertex, corner_shapes)},
+            {6, 0, vk::Format::eR32Sfloat, offsetof(vertex, line_width)},
         };
     }
 };
-}
+} // namespace tt::pipeline_box
