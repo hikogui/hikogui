928289090ca6d9b4b70a1aaa35bf5722a56c4eca
Take Vos <info@takevos.nl>
2021-11-06 17:21:39 +0100

    Example custom widget with child

---
+++ examples/custom_widgets/custom_widget_with_child_example.cpp
@@ -0,0 +1,112 @@
+// Copyright Take Vos 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#include "ttauri/GUI/gui_system.hpp"
+#include "ttauri/widgets/widget.hpp"
+#include "ttauri/widgets/label_widget.hpp"
+#include "ttauri/crt.hpp"
+
+// Every widget must inherit from tt::widget.
+class widget_with_child : public tt::widget {
+public:
+    // Every constructor of a widget starts with a `window` and `parent` argument.
+    // In most cases these are automatically filled in when calling a container widget's `make_widget()` function.
+    template<typename Label>
+    widget_with_child(tt::gui_window& window, tt::widget* parent, Label&& label) noexcept : widget(window, parent)
+    {
+        // Our child widget is a `label_widget` which requires a label to be passed as an third argument.
+        // We use a templated argument to forward the label into the `label_widget`.
+        _label_widget = std::make_unique<tt::label_widget>(window, this, std::forward<Label>(label));
+    }
+
+    // The set_constraints() function is called when the window is first initialized,
+    // or when a widget wants to change its constraints.
+    tt::widget_constraints const& set_constraints() noexcept override
+    {
+        // Almost all widgets will reset the `_layout` variable here so that it will
+        // trigger the calculations in `set_layout()` as well.
+        _layout = {};
+
+        // The constrains below have different minimum, preferred and maximum sizes.
+        auto const boxes_constraints = tt::widget_constraints{ {100, 50}, {200, 100}, {300, 100}, theme().margin };
+
+        // We need to recursively set the constraints of any child widget here as well
+        auto const label_constraints = _label_widget->set_constraints();
+
+        // We combine the boxes and label-constraints into a single constraint for this widget.
+        return _constraints = max(boxes_constraints, label_constraints);
+    }
+
+    // The `set_layout()` function is called when the window has resized, or when
+    // a widget wants to change the internal layout.
+    // 
+    // NOTE: The size of the layout may be larger than the maximum constraints of this widget.
+    void set_layout(tt::widget_layout const& context) noexcept override
+    {
+        // Update the `_layout` with the new context, in this case we want to do some
+        // calculations when the size of the widget was changed.
+        if (_layout.store(context) >= tt::layout_update::size) {
+            // Here we can do some semi-expensive calculations which must be done when resizing the widget.
+            // In this case we make two rectangles which are used in the `draw()` function.
+            _left_rectangle = tt::aarectangle{ tt::extent2{_layout.width() / 2, _layout.height()} };
+            _right_rectangle = tt::aarectangle{ tt::point2{_layout.width() / 2, 0.0}, _left_rectangle.size() };
+
+            // The layout of the child widget are also calculated here, which only needs to be done
+            // when the layout of the current widget changes.
+            _label_rectangle = align(_layout.rectangle(), _label_widget->constraints().preferred, tt::alignment::middle_center);
+        }
+
+        // The layout of any child widget must always be set, even if the layout didn't actually change.
+        // This is because child widgets may need to re-layout for other reasons.
+        _label_widget->set_layout(_label_rectangle * context);
+    }
+
+    // The `draw()` function is called when all or part of the window requires redrawing.
+    // This may happen when showing the window for the first time, when the operating-system 
+    // requests a (partial) redraw, or when a widget requests a redraw of itself.
+    void draw(tt::draw_context const& context) noexcept override
+    {
+        // We only need to draw the widget when it is visible and when the visible area of
+        // the widget overlaps with the scissor-rectangle (partial redraw) of the drawing context.
+        if (visible) {
+            if (overlaps(context, layout())) {
+                // Draw two boxes matching the rectangles calculated during set_layout().
+                // The actual RGB colors are taken from the current theme.
+                context.draw_box(_layout, _left_rectangle, theme().color(tt::theme_color::indigo));
+                context.draw_box(_layout, _right_rectangle, theme().color(tt::theme_color::blue));
+            }
+
+            // Child widget only need to be drawn when the parent is visible, but the child may have
+            // a visible area outside of the parent's visible area, therefor it should do its own
+            // overlap check.
+            _label_widget->draw(context);
+        }
+    }
+
+protected:
+    // This function is used to determine the children of this widget.
+    //
+    // The order of the children returned is used for determining the next widget for
+    // keyboard navigation.
+    [[nodiscard]] tt::pmr::generator<widget*> children(std::pmr::polymorphic_allocator<>&) const noexcept override
+    {
+        // This function is often written as a co-routine that yields a pointer to each of its children.
+        co_yield _label_widget.get();
+    }
+
+private:
+    // Child widgets are owned by their parent.
+    std::unique_ptr<tt::label_widget> _label_widget;
+    tt::aarectangle _label_rectangle;
+    tt::aarectangle _left_rectangle;
+    tt::aarectangle _right_rectangle;
+};
+
+int tt_main(int argc, char* argv[])
+{
+    auto gui = tt::gui_system::make_unique();
+    auto& window = gui->make_window(tt::l10n("Widget with child"));
+    window.content().make_widget<widget_with_child>("A1", tt::l10n("Widget with child"));
+    return gui->loop();
+}
