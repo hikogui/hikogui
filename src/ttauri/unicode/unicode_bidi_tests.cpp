4522ed6efbebc1e1460afd3b47636ccac71ad8e8
Take Vos <info@takevos.nl>
2022-01-19 01:06:24 +0100

    again

--- src/ttauri/unicode/unicode_bidi_tests.cpp
+++ src/ttauri/unicode/unicode_bidi_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
-#include "ttauri/text/unicode_bidi.hpp"
+#include "ttauri/unicode/unicode_bidi.hpp"
 #include "ttauri/file_view.hpp"
 #include "ttauri/charconv.hpp"
 #include "ttauri/ranges.hpp"
--- src/ttauri/unicode/unicode_line_break_tests.cpp
+++ src/ttauri/unicode/unicode_line_break_tests.cpp
@@ -2,8 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
-#include "ttauri/text/unicode_line_break.hpp"
-#include "ttauri/text/unicode_description.hpp"
+#include "ttauri/unicode/unicode_line_break.hpp"
+#include "ttauri/unicode/unicode_description.hpp"
 #include "ttauri/file_view.hpp"
 #include "ttauri/strings.hpp"
 #include "ttauri/coroutine.hpp"
