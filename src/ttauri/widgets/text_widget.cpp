3463908f68e8e4fe0d4b9ec3c4a7c17fda92a0a2
Take Vos <info@takevos.nl>
2022-01-22 14:39:47 +0100

    includes

--- src/ttauri/widgets/text_widget.cpp
+++ src/ttauri/widgets/text_widget.cpp
@@ -3,6 +3,8 @@
 // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 #include "text_widget.hpp"
+#include "../GUI/gui_window.hpp"
+#include "../GUI/mouse_event.hpp"
 
 namespace tt::inline v1{
 
