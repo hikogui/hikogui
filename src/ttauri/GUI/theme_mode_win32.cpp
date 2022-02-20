187b45a74711428d9ee6e6c573c69d204f4bf30d
Take Vos <info@takevos.nl>
2022-02-20 16:50:51 +0100

    Fix light theme, refactor registry access

--- src/ttauri/GUI/theme_mode_win32.cpp
+++ src/ttauri/GUI/theme_mode_win32.cpp
@@ -5,39 +5,18 @@
 #include "theme_mode.hpp"
 #include "../strings.hpp"
 #include "../log.hpp"
-#include <Windows.h>
-#undef WIN32_NO_STATUS
-#include <ntstatus.h>
-#include <winreg.h>
-#include <Uxtheme.h>
+#include "../registry_win32.hpp"
 
 namespace tt::inline v1 {
 
 [[nodiscard]] theme_mode read_os_theme_mode() noexcept
 {
-    ttlet subkey = tt::to_wstring("Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize");
-    ttlet name = tt::to_wstring("AppsUseLightTheme");
-    DWORD result;
-    DWORD result_length = sizeof(result);
-    auto status = RegGetValueW(HKEY_CURRENT_USER, subkey.c_str(), name.c_str(), RRF_RT_DWORD, NULL, &result, &result_length);
-
-    switch (status) {
-    case ERROR_SUCCESS:
-        if (result) {
-            return theme_mode::light;
-        } else {
-            return theme_mode::dark;
-        }
-
-    case ERROR_BAD_PATHNAME:
-    case ERROR_FILE_NOT_FOUND: {
-        auto reg_path = "HKEY_CURRENT_USER\\" + tt::to_string(subkey) + "\\" + tt::to_string(name);
-
-        tt_log_error("Missing {} registry entry: 0x{:08x}", reg_path, status);
-    }
+    try {
+        auto result = registry_read_current_user_dword(
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "AppsUseLightTheme");
+        return result ? theme_mode::light : theme_mode::dark;
+    } catch (...) {
         return theme_mode::light;
-
-    default: tt_log_fatal("Could get AppsUseLightTheme registry value. {:08x}", status);
     }
 }
 
--- src/ttauri/text/language_win32.cpp
+++ src/ttauri/text/language_win32.cpp
@@ -5,6 +5,7 @@
 #include "language.hpp"
 #include "../required.hpp"
 #include "../log.hpp"
+#include "../registry_win32.hpp"
 #include <Windows.h>
 #include <Winnls.h>
 
@@ -21,36 +22,17 @@ namespace tt::inline v1 {
  */
 std::vector<language_tag> language::read_os_preferred_languages() noexcept
 {
-    ttlet subkey = tt::to_wstring("Control Panel\\International\\User Profile");
-    ttlet name = tt::to_wstring("Languages");
+    try {
+        ttlet strings = registry_read_current_user_multi_string("Control Panel\\International\\User Profile", "Languages");
 
-    wchar_t result[256];
-    DWORD result_length = sizeof(result);
-
-    auto status =
-        RegGetValueW(HKEY_CURRENT_USER, subkey.c_str(), name.c_str(), RRF_RT_REG_MULTI_SZ, NULL, &result, &result_length);
-
-    switch (status) {
-    case ERROR_SUCCESS: {
-        ttlet language_names = ZZWSTR_to_string(result, result + result_length);
         auto r = std::vector<language_tag>{};
-        r.reserve(size(language_names));
-        for (ttlet &language_name : language_names) {
-            r.emplace_back(language_name);
+        r.reserve(strings.size());
+        for (ttlet &string: strings) {
+            r.push_back(language_tag{string});
         }
         return r;
-    }
 
-    case ERROR_BAD_PATHNAME:
-    case ERROR_FILE_NOT_FOUND: {
-        auto reg_path = "HKEY_CURRENT_USER\\" + tt::to_string(subkey) + "\\" + tt::to_string(name);
-        tt_log_error("Missing {} registry entry: 0x{:08x}", reg_path, status);
-        return {language_tag{"en"}};
-    }
-
-    default:
-        auto reg_path = "HKEY_CURRENT_USER\\" + tt::to_string(subkey) + "\\" + tt::to_string(name);
-        tt_log_error("Unknown error when getting {} registry value. {:08x}", reg_path, status);
+    } catch (...) {
         return {language_tag{"en"}};
     }
 }
