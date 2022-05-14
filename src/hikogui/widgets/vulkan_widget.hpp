53dc97ce6cfcab57ca93f74911408ec67ed8ba3e
Take Vos <info@takevos.nl>
2022-05-14 19:16:00 +0200

    again

--- src/hikogui/widgets/vulkan_widget.hpp
+++ src/hikogui/widgets/vulkan_widget.hpp
@@ -27,17 +27,44 @@ public:
         if (compare_store(_layout, layout)) {}
     }
 
+    /** The swap-chain is going to be teared-down.
+     *
+     * This function is called just before the swap-chain is being teared down.
+     *
+     * This requires the destruction of any references to the swap-chain's image views, including
+     * the frame-buffers created during `swapchain_build()`.
+     */
+    virtual void swapchain_teardown() {}
+
+    /** The swap-chain has been build.
+     *
+     * This function is called when either the swap-chain has just been build, or when the widget
+     * is added to a window with the swap-chain already existing.
+     *
+     * The swap-chain will also be build during resizing of the window. So this needs to be rather fast.
+     *
+     * @param views The list of swap-chain image views.
+     * @param size The size of the images in the swap-chain.
+     * @param format The pixel format of the images in the swap-chain.
+     */
+    virtual void swapchain_build(std::vector<vk::ImageView> views, vk::Extent2 size, vk::Format format) {}
+
     /** Draw using vulkan API.
      *
-     * @param view The swap-chain's image view to draw in.
-     * @param size The size of the frame buffer
-     * @param format The pixel format
+     * @param swapchain_index The index of the image-view of the swap-chain to draw into.
      * @param clipping_rectangle The rectangle on the window that is visible through this widget.
      * @param render_area The area of the window that is being drawn.
      * @param start The semaphore used to signal when the @a image_view is ready to be drawn.
      * @param finish The semaphore used to signal when the HikoGUI overlay is drawn onto the @a image_view.
      */
-    virtual void draw_vulkan(vk::ImageView view, vk::Extent2 size, vk::Format format, aarectangle clipping_rectangle, aarectangle render_area, vk::Semaphore start, vk::Semaphore finish) noexcept {}
+    virtual void draw_vulkan(
+        uint32_t swapchain_index,
+        aarectangle clipping_rectangle,
+        aarectangle render_area,
+        vk::Semaphore start,
+        vk::Semaphore finish) noexcept
+    {
+    }
 
     void draw(hi::draw_context const& context) noexcept override
     {
