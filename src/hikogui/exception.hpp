1b10604baa9707d08692e499ae82dd7da1762a9d
Take Vos <info@takevos.nl>
2022-12-27 19:17:32 +0100

    again

--- src/hikogui/exception.hpp
+++ src/hikogui/exception.hpp
@@ -9,16 +9,16 @@
 #include <exception>
 #include <stdexcept>
 #include <atomic>
+#include <bit>
+#include <format>
 
 #pragma once
 
-namespace hi {
-inline namespace v1 {
+namespace hi { inline namespace v1 {
 
 /** Message to show when the application is terminated.
  */
 inline std::atomic<char const *> terminate_message = nullptr;
-
 
 /** Set the message to display when the application terminates.
  *
@@ -44,7 +44,6 @@ inline std::terminate_handler old_terminate_handler;
  */
 [[noreturn]] void terminate_handler() noexcept;
 
-
 /** Exception thrown during parsing on an error.
  * This exception is often thrown due to an error in the syntax
  * in both text and binary files.
@@ -61,6 +60,99 @@ inline std::terminate_handler old_terminate_handler;
 class parse_error : public std::runtime_error {
 public:
     using std::runtime_error::runtime_error;
+
+    /** Create a parse error at a specific point in UTF-8 encoded text.
+     *
+     * @param first An iterator to the start of the text.
+     * @param last An iterator to the position of the character where the error occurred.
+     * @param tab_size The number of spaces a tab character indents.
+     * @param msg A std::format msg.
+     * @param args The arguments for std::format.
+     */
+    template<typename It, typename... Args>
+    [[nodiscard]] constexpr parse_error(It first, It last, int tab_size, char const *msg, Args const&...args) noexcept :
+        runtime_error(make_what(first, last, tab_size, msg, args...))
+    {
+    }
+
+    /** Create a parse error at a specific point in UTF-8 encoded text.
+     *
+     * @param first An iterator to the start of the text.
+     * @param last An iterator to the position of the character where the error occurred.
+     * @param msg A std::format msg.
+     * @param args The arguments for std::format.
+     */
+    template<typename It, typename... Args>
+    [[nodiscard]] constexpr parse_error(It first, It last, char const *msg, Args const&...args) noexcept :
+        parse_error(first, last, 8, msg, args...)
+    {
+    }
+
+    /** Get the line and column position of an iterator in a UTF-8 string.
+     *
+     * @param first An iterator to the first character of the text.
+     * @param last An iterator to the character for which the position need to be found.
+     * @param tab_size The number of spaces of indentation for a tab-character.
+     * @return Zero based indices for the line and column number.
+     */
+    template<typename It>
+    [[nodiscard]] static constexpr std::pair<size_t, size_t> get_line_position(It first, It last, size_t tab_size) noexcept
+    {
+        auto line_nr = 0_uz;
+        auto column_nr = 0_uz;
+
+        auto c32 = char32_t{};
+        auto continue_count = int{};
+        while (first != last) {
+            hilet c8 = char_cast<char8_t>(*first++);
+
+            if ((c8 & 0xc0) == 0x80) {
+                --continue_count;
+                c32 <<= 6;
+                c32 |= c8 & 0x3f;
+
+            } else if (c8 & 0x80) {
+                continue_count = std::countl_one(c8) - 1;
+                c32 = c8 & (0b00'111'111 >> continue_count);
+
+            } else {
+                continue_count = 0;
+                c32 = c8;
+            }
+
+            if (not continue_count) {
+                ++column_nr;
+                switch (c32) {
+                case '\n':
+                case '\v':
+                case '\f':
+                case U'\u0085':
+                case U'\u2028':
+                case U'\u2029':
+                    ++line_nr; [[fallthrough]]
+                case '\r':
+                    column_nr = 0;
+                    break;
+                case '\t':
+                    column_nr /= tab_size;
+                    column_nr *= tab_size;
+                    break;
+                default:;
+                }
+            }
+        }
+
+        return {line_nr, column_nr};
+    }
+
+private:
+    template<typename It, typename... Args>
+    [[nodiscard]] static constexpr std::string
+    make_what(It first, It last, size_t tab_size, char const *msg, Args const&...args) noexcept
+    {
+        hilet[line_nr, column_nr] = get_line_position(first, last, tab_size);
+        return {std::format("{}:{}: {}", line_nr + 1, column_nr + 1, std::format(msg, args...))};
+    }
 };
 
 /** Exception thrown during execution of a dynamic operation.
@@ -127,4 +219,4 @@ public:
     using std::runtime_error::runtime_error;
 };
 
-}} // namespace hi::inline v1
+}} // namespace hi::v1
--- src/hikogui/lexer.hpp
+++ src/hikogui/lexer.hpp
@@ -1,22 +1,22 @@
+// Copyright Take Vos 2022.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
 
 #include "unicode/unicode_identifier.hpp"
 #include "char_maps/utf_8.hpp"
 
-namespace hi {
-inline namespace v1 {
+namespace hi { inline namespace v1 {
 
 template<typename It, typename EndIt>
 class lexer {
 public:
     class token_type {
-
     private:
         // id,string-literal,
-        std::variant<std::string,std::string,size_t> _v;
+        std::variant<std::string, std::string, size_t> _v;
     };
-
 
     class const_iterator {
     public:
@@ -25,7 +25,7 @@ public:
             parse_token();
         }
 
-        constexpr const_iterator &operator++() const noexcept
+        constexpr const_iterator& operator++() const noexcept
         {
             parse_token();
             return *this;
@@ -39,133 +39,160 @@ public:
     private:
         It _it;
         EndIt _end;
-        char number_group_set = ',';
-        size_t tab_size = 8;
+        char _number_group_separator = ',';
+        size_t _tab_size = 8;
 
         lexer *_lexer = nullptr;
         bool _finished = false;
+        std::string _buffer = {};
 
-        [[nodiscard]] constexpr static std::pair<size_t, size_t> line_count(It first, It last, size_t tab_size) noexcept
+        [[nodiscard]] constexpr std::optional<token_type> parse_string() noexcept
         {
-            auto line_nr = 0_uz;
-            auto column_nr = 0_uz;
 
-            while (first != last) {
-                hilet [c32, good] = char_map<"utf-8">{}.read(first, last);
-                if (c32 == U'\n' or c32 == U'\v' or c32 == U'\f' or c32 == U'\u0085' or c32 == U'\u2028' or c32 == U'\u2029') {
-                    column_nr = 0;
-                    ++line_nr;
-                } else if (c32 == U'\r') {
-                    column_nr = 0;
-                } else if (c32 == U'\t') {
-                    ++column_nr;
-                    column_nr \= tab_size;
-                    column_nr *= tab_size;
-                } else {
-                    ++column_nr;
-                }
-            }
-
-            return {line_nr, column_nr};
         }
 
-        [[nodiscard]] constexpr token_type parse_id(It first) noexcept
+        [[nodiscard]] constexpr std::optional<token_type> parse_id() noexcept
         {
-           while (_it != _end) {
+            hi_axiom(_it != end);
+
+            hilet first = _it;
+
+            // The first character of the ID does not include digits.
+            if (hilet[c, good] = char_map<"utf-8">{}.read(_it, _end); not is_ID_Start(c)) {
+                // This was not a ID character, rewind.
+                _it = first;
+                return std::nullopt;
+            }
+
+            while (_it != _end) {
                 hilet prev_it = _it;
-                hilet [c, good] = char_map<"utf-8"{}.read(_it, _end);
-                if (not is_ID_Continue(c)) {
+                if (hilet[c, good] = char_map<"utf-8">{}.read(_it, _end); not is_ID_Continue(c)) {
+                    // This was not a ID character, rewind this last character.
                     _it = prev_it;
-                    return {first, _it};
+                    return token_type::id(first, _it);
                 }
             }
-            return token_type::id(first, _end);
+
+            // ID at end-of-text.
+            return token_type::id(first, _it);
         }
 
-        [[nodiscard]] constexpr static bool is_number_first(char c) noexcept
+        [[nodiscard]] constexpr std::optional<token_type> parse_number() noexcept
         {
-            return (c >= '0' and c <= '9') or c == '.';
-        }
+            enum class state_type { integer, floating_point, now_exponent, has_exponent };
 
-        [[nodiscard]] constexpr static bool is_number_continue(char c) noexcept
-        {
-            return is_number_first(c) or c == number_group_sep;
-        }
+            hi_axiom(_it != _end);
+            hilet first = it;
+            float_state state = state_type::integer;
 
-        [[nodiscard]] constexpr static bool is_number_second(char first_c, char c) noexcept
-        {
-            // clang-format off
-            return
-                is_number_continue(c) or (first_c = '0' and
-                    (c == 'x' or c == 'X' or c == 'd' or c == 'D' or c == 'o' or c == 'O' or c == 'b' or c == 'B'));
-            // clang-format on
-        }
-
-        [[nodiscard]] constexpr token_type parse_number(It first, char first_c) noexcept
-        {
-            ++it;
-
-            if (_it == _end) {
-                return {first, _it};
+            // Check first character of number.
+            if (*_it == '.') {
+                state = state_type::floating_point;
+            } else if (not(*_it >= '0' and *_it <= '9')) {
+                return std::nullopt;
             }
 
-            if (not is_number_second(first_c, *_it)) {
-                return {first, _it};
-            }
-            ++_it;
+            // Reuse the allocation of _buffer.
+            _buffer.clear();
+            _buffer += *it;
 
-            while (true) {
-                if (_it == _end or not is_number_continue(*_it)) {
-                    return {first, _it};
+            // Check the continues characters of a number.
+            bool is_hex = false;
+            for (++_it; it != _end; ++_it) {
+                hilet c = *_it;
+                if ((c >= '0' and c <= '9')) {
+                    _buffer += c;
+                    state = (state == state_type::now_exponent) ? state_type::has_exponent : state;
+
+                } else if (((c >= 'a' and c <= 'f') or (c >= 'A' and c <= 'F')) and is_hex) {
+                    // The letters 'a' to 'f' or 'A' to 'F' may only appear in hexadecimal numbers.
+                    _buffer += c;
+                    state = (state == state_type::now_exponent) ? state_type::has_exponent : state;
+
+                } else if (c == _number_group_separator) {
+                    // Don't add the number-group-separator to the buffer, which `from_chars()` can't parse.
+                    state = (state == state_type::now_exponent) ? state_type::has_exponent : state;
+
+                } else if (c == '.' and state < state_type::floating_point) {
+                    // A single '.' will make this a floating-point number, and can appear at most once.
+                    _buffer += c;
+                    state = state_type::floating_point;
+
+                } else if ((c == 'x' or c == 'X') and _buffer == "0") {
+                    // A hexadecimal marker may only appear directly after the first digit when that digit is '0'.
+                    _buffer += c;
+                    is_hex = true;
+
+                } else if ((c == 'b' or c == 'b' or c == 'o' or c == 'O' or c == 'd' or c == 'D') and _buffer == "0") {
+                    // A binary/octal/decimal marker may only appear directly after the first digit when that digit is '0'.
+                    _buffer += c;
+
+                } else if ((c == 'e' or c == 'E') and state < state_type::now_exponent) {
+                    // A single 'e' or 'E' will make this a floating-point number with an exponent, and can appear at most once.
+                    _buffer += c;
+                    state = state_type::now_exponent;
+
+                } else if ((c == '-' or c == '+') and state == state_type::now_exponent) {
+                    // A '+' or '-' may only appear directly after the exponent marker 'e' or 'E'.
+                    _buffer += c;
+                    state = state_type::has_exponent;
+
+                } else if (state >= state_type::floating_point) {
+                    // end-of-number.
+                    if (_buffer == ".") {
+                        // Rewind and reinterpret as an operator.
+                        _it = first;
+                        return std::nullopt;
+                    } else {
+                        // Floating-point with an optional suffix.
+                        return token_type::floating_point{first, _buffer, parse_id()};
+                    }
+
+                } else {
+                    // Integer with an optional suffix.
+                    return token_type::integer{first, _buffer, parse_id()};
                 }
+            }
 
-                ++_it;
+            // The number was at end-of-text.
+            if (state >= state_type::floating_point) {
+                if (_buffer == ".") {
+                    // Rewind and reinterpret as an operator.
+                    _it = first;
+                    return std::nullopt;
+                } else {
+                    return token_type::floating_point{first, _buffer};
+                }
+            } else {
+                return token_type::integer{first, _buffer};
             }
         }
 
-        [[nodiscard]] constexpr token_type parse_token() noexcept
+        [[nodiscard]] constexpr token_type parse_token()
         {
             while (_it != _end) {
-                hilet c = *_it;
-
                 // For performance try ASCII first.
-                if (is_Patern_White_Space(char_cast<char32_t>(c)) {
-                    // Skip over whitespace
-                    ++_it;
-                    continue;
+                if (parse_white_space()) {
+                    continue; // Skip over whitespace
 
-                } else if (is_Pattern_Syntax(char_cast<char32_t>(c))) {
-                    return parse_syntax(c);
+                } else if (auto syntax_token = parse_syntax()) {
+                    return *syntax_token;
 
-                } else if (is_number_first(_it, c)) {
-                    return parse_number(c);
+                } else if (auto number_token = parse_number()) {
+                    return *number_token;
 
-                } else if (c == '"' or c == '\'') {
-                    return parse_string(c);
+                } else if (auto string_token = parse_string()) {
+                    return *string_token;
 
-                } else if (std::find(comment_start
+                } else if (auto comment_token = parse_comment()) {
+                    return *comment_token;
 
-                } else if (is_ID_Start(char_cast<char32_t>(c))) {
-                    return parse_id(_it);
+                } else if (auto id_token = parse_id()) {
+                    return *id_token;
 
                 } else {
-                    hilet prev_it = _it;
-                    hilet [c32, good] = char_map<"utf-8">{}.read(_it, _end);
-
-                    if (is_Patern_White_Space(c32)) {
-                        // Skip over whitespace
-                        continue;
-
-                    } else if (is_Pattern_Syntax(c32)) {
-                        return parse_syntax(c);
-
-                    } else if (is_ID_Start(c32)) {
-                        return parse_id(prev_it);
-                        
-                    } else {
-                        hilet [line_nr, column_nr] = line_count(_begin, _it);
-                        throw parse_error(std::format("Unexpected character U+{:04x} at {}:{}", char_cast<uint32_t>(c32), line_nr + 1, column_nr + 1));
-                    }
+                    hilet[c, good] = char_set<"utf-8">{}.read(_it, _end);
+                    throw parse_error(_begin, _it, _tab_size, "Unexpected character U+{:04x}.", char_cast<uint32_t>(c32));
                 }
             }
 
@@ -173,7 +200,6 @@ public:
             return {};
         }
     };
-
 
     constexpr lexer(It first, EndIt last) noexcept : _first(first), _last(last) {}
 
@@ -189,8 +215,6 @@ public:
 
 private:
     It _begin;
-    EndIt _end; 
+    EndIt _end;
 };
-
-}}
-
+}} // namespace hi::v1
