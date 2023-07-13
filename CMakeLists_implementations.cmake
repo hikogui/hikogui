f6a037b61e4a58516294026ee67a212e80447297
Take Vos <info@takevos.nl>
2023-07-13 18:03:44 +0200

    Remove time cpp file

--- CMakeLists_implementations.cmake
+++ CMakeLists_implementations.cmake
@@ -99,7 +99,6 @@ target_sources(hikogui PRIVATE
     ${HIKOGUI_SOURCE_DIR}/text/text_shaper_impl.cpp
     ${HIKOGUI_SOURCE_DIR}/text/text_shaper_line_impl.cpp
     ${HIKOGUI_SOURCE_DIR}/text/text_style_impl.cpp
-    ${HIKOGUI_SOURCE_DIR}/time/time_stamp_utc_impl.cpp
     $<$<PLATFORM_ID:Windows>:${HIKOGUI_SOURCE_DIR}/utility/debugger_win32_impl.cpp>
     $<$<PLATFORM_ID:Windows>:${HIKOGUI_SOURCE_DIR}/utility/exception_win32_impl.cpp>
 ) 
