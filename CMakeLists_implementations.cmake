579950ab7fd9c15ff686adcee093c9597ae6d420
Take Vos <info@takevos.nl>
2023-09-04 17:03:06 +0200

    skeleton nocpp

--- CMakeLists_implementations.cmake
+++ CMakeLists_implementations.cmake
@@ -1,7 +1,5 @@
 
 target_sources(hikogui PRIVATE
-    ${HIKOGUI_SOURCE_DIR}/skeleton/skeleton_impl.cpp
-    ${HIKOGUI_SOURCE_DIR}/skeleton/skeleton_parse_context_impl.cpp
     ${HIKOGUI_SOURCE_DIR}/text/text_shaper_char_impl.cpp
     ${HIKOGUI_SOURCE_DIR}/text/text_shaper_impl.cpp
     ${HIKOGUI_SOURCE_DIR}/text/text_shaper_line_impl.cpp
