28e0d8bda5469093843dd04bba7d073b0f36c520
Take Vos <info@takevos.nl>
2024-02-17 17:13:49 +0100

    doc

--- src/hikogui/dispatch/dispatch.hpp
+++ src/hikogui/dispatch/dispatch.hpp
@@ -20,4 +20,26 @@
 #include "task.hpp" // export
 #include "when_any.hpp" // export
 
+/** @module hikogui.dispatch
+ * @brief Asynchronous dispatching and co-routine tasks.
+ *
+ * The Dispatch Loop
+ * -----------------
+ * Each thread has a local dispatch loop, which can be accessed using
+ * the static function `hi::loop::local()`, which will return a `hi::loop`
+ * instance.
+ *
+ * There are also two well-known loops which can be accessed using the
+ * static functions `hi::loop::main()` and `hi::loop::timer()`.
+ * 
+ * The main-loop is used to handle events from the GUI, Audio-control and
+ * network. The main-loop is latency sensitive and any event should be
+ * handled quickly.
+ *
+ * The timer-loop is less latency sensitive and can be used for slower
+ * maintenace tasks, such as high-resolution clock synchronization and logging
+ * of telemetry.
+ *
+ * 
+ */
 hi_export_module(hikogui.dispatch);
